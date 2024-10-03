410bdeea5f3c3d7855e30e56865b2d13609d7590
elhamod <55500282+elhamod@users.noreply.github.com>
2024-10-03 03:03:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -94,7 +94,6 @@ chain_with_history = RunnableWithMessageHistory(
 for msg in msgs.messages:
     if (msg.type in ["ai", "human"]):
             st.chat_message(msg.type).write(msg.content)
-            st.chat_message(msg.type).write("hi")
 
 if prompt := st.chat_input():
     # Add human message
