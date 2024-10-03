f4207e04ac1a697027e8352c4154551014fe8caa
elhamod <55500282+elhamod@users.noreply.github.com>
2024-10-03 19:34:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,18 +3,20 @@ from gcsa.google_calendar import GoogleCalendar
 from gcsa.recurrence import Recurrence, DAILY, SU, SA
 from google.oauth2 import service_account
 
-from beautiful_date import Jan, Apr, Sept
+from beautiful_date import Jan, Apr, Sept, Oct
 import json
 import os
+from datetime import date, datetime
+from beautiful_date import hours
 
-import streamlit as st
 
 from langchain_core.runnables.utils import ConfigurableFieldSpec
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
-from langgraph.prebuilt import create_react_agent
+# from langgraph.prebuilt import create_react_agent
+from langchain.agents.react.agent import create_react_agent
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.tools import Tool  # Use the Tool object directly
+from langchain_core.tools import Tool, StructuredTool  # Use the Tool object directly
 from langchain_openai import ChatOpenAI
 from langchain.agents import AgentExecutor, create_tool_calling_agent
 from langchain.agents import initialize_agent
@@ -26,6 +28,9 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI
 from langchain.callbacks.tracers import ConsoleCallbackHandler
+from pydantic import BaseModel, Field
+
+from google.colab import userdata
 
 
 
@@ -41,17 +46,25 @@ credentials = service_account.Credentials.from_service_account_info(
 calendar = GoogleCalendar(credentials=credentials)
 
 
+
 #-------
 ### IMPORTANT: Here is an example of a listing event tool. For other features, replicate this code and edit
 
+class GetEventargs(BaseModel):
+    from_datetime: datetime = Field(description="beginning of date range to retrieve events")
+    to_datetime: datetime = Field(description="end of date range to retrieve events")
+
 # Define the tool 
-def get_events(dummy):
-    return list(calendar.get_events(calendar_id="mndhamod@gmail.com"))
+def get_events(from_datetime, to_datetime):
+    events = calendar.get_events(calendar_id="mndhamod@gmail.com", time_min=from_datetime, time_max=to_datetime)
+    # print(list(events), from_datetime, to_datetime)
+    return list(events)
 
 # Create a Tool object 
-list_event_tool = Tool(
+list_event_tool = StructuredTool(
     name="GetEvents",
     func=get_events,
+    args_schema=GetEventargs,
     description="Useful for getting the list of events from the user's calendar."
 )
 
@@ -61,22 +74,25 @@ list_event_tool = Tool(
 ### IMPORTANT: For addign event: 
 
 # Define the tool 
-def add_event(start_date, start_time, length_hours):
-    from beautiful_date import hours
-    start = (start_date)[start_time]
+def add_event(start_date_time, length_hours, event_name):
+    start = start_date_time
     end = start + length_hours * hours
-    event = Event('Meeting',
+    event = Event(event_name,
                   start=start,
                   end=end)
-    r = calendar.add_event(event)
-    st.write(r)
-    return r
+    return calendar.add_event(event, calendar_id="mndhamod@gmail.com")
 
 # Create a Tool object 
-add_event_tool = Tool(
+class AddEventargs(BaseModel):
+    start_date_time: datetime = Field(description="start date and time of event")
+    length_hours: int = Field(description="length of event")
+    event_name: str = Field(description="name of the event")
+
+add_event_tool = StructuredTool(
     name="AddEvent",
     func=add_event,
-    description="Useful for adding an event with a start date, start time, and length in hours the list of events. Returns whethere addition was successful or failed."
+    args_schema=AddEventargs,
+    description="Useful for adding an event with a start date, event name, and length in hours the list of events. Calls get_events to make sure if the new event has been deleted."
 )
 
 #------------
@@ -87,13 +103,29 @@ tools = [list_event_tool, add_event_tool]
 # Create the LLM
 llm = ChatOpenAI(api_key=st.secrets["OPENAI_API_KEY"], temperature=0.1)
 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful Google Calendar assistant"),
+        ("human", "{input}"),
+        # Placeholders fill up a **list** of messages
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+)
 
-agent = create_react_agent(llm, tools )
-agent = AgentExecutor.from_agent_and_tools(
+
+agent = create_tool_calling_agent(llm, tools, prompt)
+# agent = create_react_agent(llm, tools, prompt=prompt)
+
+# agent = create_react_agent(llm, tools )
+agent = AgentExecutor(
     agent=agent,  # type: ignore
     tools=tools,
     verbose=True,
+    # agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,  # Ensure this is a valid AgentType
+    # tools=tools,
+    # llm=llm,
 )
+
 
 
 #--------------------
